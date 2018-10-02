7edbc7d1d2e769d070b623be2054eebed38ee2e5
Unknown <sander.de.boer4798@hotmail.com>
2018-10-02 16:23:39 +0200

    Created the repository abstractions.

---
+++ repository.abstraction/src/main/java/com/gambleton/repository/abstraction/UserContext.java
@@ -0,0 +1,12 @@
+package com.gambleton.repository.abstraction;
+
+import com.gambleton.models.User;
+
+public interface UserContext extends Context<User> {
+    /**
+     * @param username the username of the user you'd like to get
+     * @param password the password of the user you'd like to get
+     * @return the user you wanted.
+     */
+    User getByCredentials(String username, String password);
+}
