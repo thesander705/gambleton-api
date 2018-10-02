5bc75a7d308b24520254f13a52a45a91504fd09b
Unknown <sander.de.boer4798@hotmail.com>
2018-10-02 11:29:22 +0200

    Created the presentation layer

---
+++ presentation/src/main/java/com/gambleton/presentation/PresentationApplication.java
@@ -0,0 +1,12 @@
+package com.gambleton.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentationApplication.class, args);
+    }
+}
