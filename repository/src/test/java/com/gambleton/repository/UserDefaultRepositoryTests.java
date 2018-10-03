5e2e3b1f140dd43563135d46bd9efd383779624b
Unknown <sander.de.boer4798@hotmail.com>
2018-10-03 12:31:50 +0200

    Created a test class for the UserDefaultRepository

---
+++ repository/src/test/java/com/gambleton/repository/UserDefaultRepositoryTests.java
@@ -0,0 +1,11 @@
+package com.gambleton.repository;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UserDefaultRepositoryTests {
+    @Test
+    public void testTrue(){
+        assertEquals(true, true);
+    }
+}
