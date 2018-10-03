d10b814f92b4aacfd609ff40bba6c99dcbdfc361
Unknown <sander.de.boer4798@hotmail.com>
2018-10-03 14:47:57 +0200

    Created the api posibility.

--- presentation/src/main/java/com/gambleton/presentation/UserController.java
+++ presentation/src/main/java/com/gambleton/presentation/UserController.java
@@ -1,14 +1,19 @@
 package com.gambleton.presentation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.gambleton.factory.Factory;
+import com.gambleton.logic.abstraction.UserLogic;
+import com.gambleton.models.User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/user")
-    public String index() {
-        return "asd";
+    @PostMapping("/userByCredentials")
+    public User getUserByCredentials(String username, String password) {
+        UserLogic userLogic = Factory.getUserLogic();
+        User user = userLogic.getByCredentials(username, password);
+        return user;
     }
 }
