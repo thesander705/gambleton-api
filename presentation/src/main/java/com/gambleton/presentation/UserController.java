36fcf9c14347f30a191e1bdceca3ce61f18db8f7
Unknown <sander.de.boer4798@hotmail.com>
2018-10-24 17:23:43 +0200

    Created a viewmodel for the credentials.

--- presentation/src/main/java/com/gambleton/presentation/UserController.java
+++ presentation/src/main/java/com/gambleton/presentation/UserController.java
@@ -3,9 +3,11 @@ package com.gambleton.presentation;
 import com.gambleton.factory.Factory;
 import com.gambleton.logic.abstraction.UserLogic;
 import com.gambleton.models.User;
+import com.gambleton.presentation.viewModels.userController.Credentials;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping("/userByCredentials")
-    public ResponseEntity<Object> getUserByCredentials(String username, String password) {
+    public ResponseEntity<Object> getUserByCredentials(@RequestBody Credentials credentials) {
         UserLogic userLogic = Factory.getUserLogic();
-        User user = userLogic.getByCredentials(username, password);
+        User user = userLogic.getByCredentials(credentials.getUsername(), credentials.getPassword());
 
         if (user == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
