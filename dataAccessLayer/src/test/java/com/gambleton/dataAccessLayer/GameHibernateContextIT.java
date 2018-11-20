7c1860c28d5b1b74423e83a92c47d29ccc670a1e
Unknown <sander.de.boer4798@hotmail.com>
2018-11-20 17:30:42 +0100

    Added a test to check if a game can be created.

--- dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/GameHibernateContextIT.java
+++ dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/GameHibernateContextIT.java
@@ -1,10 +1,14 @@
 package com.gambleton.dataAccessLayer;
 
+import com.gambleton.models.Game;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class GameHibernateContextIT {
     private SessionFactory sessionFactory;
@@ -32,5 +36,28 @@ public class GameHibernateContextIT {
         }
 
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void createCreatesAUser(){
+        Game game = new Game();
+        game.setName("Test game");
+        game.setDescription("This is a test game");
+
+        gameHibernateContext.create(game);
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        ArrayList<Game> games = (ArrayList<Game>) session.createQuery("from Game").list();
+
+        session.getTransaction().commit();
+
+        if (games != null && !games.isEmpty()){
+            Assert.assertTrue(true);
+            return;
+        }
+
+        Assert.fail();
     }
 }
