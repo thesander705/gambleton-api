815aeaa8bbb9a8170e1b79d426a2acd6355e28c2
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-11-28 13:35:10 +0100

    Added a test to check if a game gets created.

--- repository/src/test/java/com/gambleton/repository/GameDefaultRepositoryTest.java
+++ repository/src/test/java/com/gambleton/repository/GameDefaultRepositoryTest.java
@@ -1,4 +1,32 @@
 package com.gambleton.repository;
 
+import com.gambleton.dataAccessLayer.abstraction.GameContext;
+import com.gambleton.dataAccessLayer.abstraction.UserContext;
+import com.gambleton.models.Game;
+import com.gambleton.repository.abstraction.GameRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class GameDefaultRepositoryTest {
+    @Test
+    public void createCheckIfGameGetsCreated() {
+        GameContext gameContext = mock(GameContext.class);
+
+        Game gameToCreate = new Game();
+        gameToCreate.setName("Test game");
+        gameToCreate.setDescription("testGame");
+
+        ArgumentCaptor<Game> argument = ArgumentCaptor.forClass(Game.class);
+
+        GameRepository gameRepository = new GameDefaultRepository(gameContext);
+        gameRepository.create(gameToCreate);
+
+        verify(gameContext).create(argument.capture());
+
+        Assert.assertNotNull(argument.getValue());
+    }
 }
