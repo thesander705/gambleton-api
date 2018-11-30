4590e9b4534fdb7ebf38a3407e3f540ff140ab66
Unknown <sander.de.boer4798@hotmail.com>
2018-11-30 08:19:25 +0100

    Added a competitor model.

---
+++ models/src/main/java/com/gambleton/models/Competitor.java
@@ -0,0 +1,40 @@
+package com.gambleton.models;
+
+public class Competitor {
+    private int id;
+    private String name;
+    private String description;
+    private Game game;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+}
