333ce88a169041a7897d60992ae0cfbb8ae539e8
Unknown <sander.de.boer4798@hotmail.com>
2018-11-30 17:37:57 +0100

    Created constructors for the MatchHibernateContext

--- dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/MatchHibernateContext.java
+++ dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/MatchHibernateContext.java
@@ -2,13 +2,34 @@ package com.gambleton.dataAccessLayer;
 
 import com.gambleton.dataAccessLayer.abstraction.MatchContext;
 import com.gambleton.models.Match;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
 public class MatchHibernateContext implements MatchContext {
+    private final SessionFactory sessionFactory;
+
+    public MatchHibernateContext() {
+        this("hibernate.cfg.xml");
+    }
+
+    MatchHibernateContext(String filePath){
+        try {
+            this.sessionFactory = new Configuration().configure(filePath).buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    MatchHibernateContext(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public void create(Match entity) {
-        
+
     }
 
     @Override
