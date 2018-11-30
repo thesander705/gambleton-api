a2eb7aec0d39c095b91ca8d922d8be959f32cdd8
Unknown <sander.de.boer4798@hotmail.com>
2018-11-30 17:38:19 +0100

    Created setup for the tests.

--- dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/MatchHibernateContextIT.java
+++ dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/MatchHibernateContextIT.java
@@ -1,4 +1,32 @@
 package com.gambleton.dataAccessLayer;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+
 public class MatchHibernateContextIT {
+    private SessionFactory sessionFactory;
+    private MatchHibernateContext matchHibernateContext;
+
+    @Before
+    public void setupEachTest() {
+        try {
+            this.sessionFactory = new Configuration().configure("hibernate-test.cfg.xml").buildSessionFactory();
+        } catch (Exception ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        this.matchHibernateContext = new MatchHibernateContext(this.sessionFactory);
+    }
+
+    @After
+    public void afterEachTest() {
+        this.sessionFactory.close();
+    }
+
+    public void createCreatesAMatch(){
+
+    }
 }
