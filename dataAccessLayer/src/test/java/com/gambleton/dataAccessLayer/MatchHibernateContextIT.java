7b05faf7c4173af9b9ecbf621c3e09acd23897a7
Unknown <sander.de.boer4798@hotmail.com>
2018-12-04 07:26:55 +0100

    Created a test to test if the create method creates a match.

--- dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/MatchHibernateContextIT.java
+++ dataAccessLayer/src/test/java/com/gambleton/dataAccessLayer/MatchHibernateContextIT.java
@@ -1,9 +1,17 @@
 package com.gambleton.dataAccessLayer;
 
+import com.gambleton.models.BetOption;
+import com.gambleton.models.Competitor;
+import com.gambleton.models.Match;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class MatchHibernateContextIT {
     private SessionFactory sessionFactory;
@@ -26,7 +34,26 @@ public class MatchHibernateContextIT {
         this.sessionFactory.close();
     }
 
+    @Test
     public void createCreatesAMatch(){
+        Match match = new Match();
+        match.setTitle("Test match");
+        match.setDescription("This is a test match");
 
+        matchHibernateContext.create(match);
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        ArrayList<Match> matchs = (ArrayList<Match>) session.createQuery("from Match").list();
+
+        session.getTransaction().commit();
+
+        if (matchs != null && !matchs.isEmpty()){
+            Assert.assertTrue(true);
+            return;
+        }
+
+        Assert.fail();
     }
 }
