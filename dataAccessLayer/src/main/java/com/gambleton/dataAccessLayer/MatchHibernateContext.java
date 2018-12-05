719325c2a588a4f46bb4380f1f73cd526807968c
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-05 16:41:55 +0100

    Made all tests pass.

--- dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/MatchHibernateContext.java
+++ dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/MatchHibernateContext.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatchHibernateContext implements MatchContext {
@@ -53,16 +54,35 @@ public class MatchHibernateContext implements MatchContext {
 
     @Override
     public List<Match> getAll() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        ArrayList<Match> matches = (ArrayList<Match>) session.createQuery("from Match").list();
+
+        session.getTransaction().commit();
+        return matches;
     }
 
     @Override
     public void update(Match entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
 
+        Match toUpdate = session.get(Match.class, entity.getId());
+        toUpdate.setTitle(entity.getTitle());
+        toUpdate.setDescription(entity.getDescription());
+
+        session.update(toUpdate);
+        session.getTransaction().commit();
     }
 
     @Override
     public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
 
+        Match toDelete = session.get(Match.class, id);
+        session.delete(toDelete);
+        session.getTransaction().commit();
     }
 }
