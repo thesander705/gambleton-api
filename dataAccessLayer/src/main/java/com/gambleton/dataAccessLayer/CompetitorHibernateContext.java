516dd83d5f3df52d47bad0f22cda6c140aaa6a18
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-08 12:33:36 +0100

    Made all tests pass.

--- dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/CompetitorHibernateContext.java
+++ dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/CompetitorHibernateContext.java
@@ -2,9 +2,11 @@ package com.gambleton.dataAccessLayer;
 
 import com.gambleton.dataAccessLayer.abstraction.CompetitorContext;
 import com.gambleton.models.Competitor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompetitorHibernateContext implements CompetitorContext {
@@ -28,28 +30,60 @@ public class CompetitorHibernateContext implements CompetitorContext {
         this.sessionFactory = sessionFactory;
     }
 
+
     @Override
     public void create(Competitor entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
 
+        session.save(entity);
+
+        session.getTransaction().commit();
     }
 
     @Override
     public Competitor get(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+
+        Competitor competitor = session.get(Competitor.class, id);
+        session.getTransaction().commit();
+
+        return competitor;
     }
 
     @Override
     public List<Competitor> getAll() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        ArrayList<Competitor> competitors = (ArrayList<Competitor>) session.createQuery("from Competitor").list();
+
+        session.getTransaction().commit();
+        return competitors;
     }
 
     @Override
     public void update(Competitor entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
 
+        Competitor toUpdate = session.get(Competitor.class, entity.getId());
+        toUpdate.setName(entity.getName());
+        toUpdate.setDescription(entity.getDescription());
+
+        session.update(toUpdate);
+        session.getTransaction().commit();
     }
 
     @Override
     public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
 
+        Competitor toDelete = session.get(Competitor.class, id);
+        session.delete(toDelete);
+        session.getTransaction().commit();
     }
 }
