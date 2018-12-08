dbf1848f7e086891db3ab073dd185affc8b9cf1a
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-08 12:30:52 +0100

    Added multiple constructors in the competitorContext.

--- dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/CompetitorHibernateContext.java
+++ dataAccessLayer/src/main/java/com/gambleton/dataAccessLayer/CompetitorHibernateContext.java
@@ -2,10 +2,32 @@ package com.gambleton.dataAccessLayer;
 
 import com.gambleton.dataAccessLayer.abstraction.CompetitorContext;
 import com.gambleton.models.Competitor;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
 public class CompetitorHibernateContext implements CompetitorContext {
+
+    private final SessionFactory sessionFactory;
+
+    public CompetitorHibernateContext() {
+        this("hibernate.cfg.xml");
+    }
+
+    CompetitorHibernateContext(String filePath){
+        try {
+            this.sessionFactory = new Configuration().configure(filePath).buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    CompetitorHibernateContext(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public void create(Competitor entity) {
 
