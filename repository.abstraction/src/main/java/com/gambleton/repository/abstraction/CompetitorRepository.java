ff3bede7473ddf44003a372dbfdfe2ee5b615456
Unknown <sander.de.boer4798@hotmail.com>
2018-12-10 07:23:17 +0100

    Added a match and competitor repoistory

---
+++ repository.abstraction/src/main/java/com/gambleton/repository/abstraction/CompetitorRepository.java
@@ -0,0 +1,6 @@
+package com.gambleton.repository.abstraction;
+
+import com.gambleton.models.Competitor;
+
+public interface CompetitorRepository extends Repository<Competitor> {
+}
---
+++ repository.abstraction/src/main/java/com/gambleton/repository/abstraction/MatchRepository.java
@@ -0,0 +1,6 @@
+package com.gambleton.repository.abstraction;
+
+import com.gambleton.models.Match;
+
+public interface MatchRepository extends Repository<Match> {
+}
