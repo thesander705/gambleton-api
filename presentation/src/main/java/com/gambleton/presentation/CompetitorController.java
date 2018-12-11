5a26c83ac4045094ecbafcc3d228f19b39df04f3
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-11 13:17:08 +0100

    Added cache in the competitor controller.

--- presentation/src/main/java/com/gambleton/presentation/CompetitorController.java
+++ presentation/src/main/java/com/gambleton/presentation/CompetitorController.java
@@ -4,6 +4,9 @@ import com.gambleton.factory.Factory;
 import com.gambleton.logic.abstraction.CompetitorLogic;
 import com.gambleton.models.Competitor;
 import com.gambleton.presentation.viewModels.competitorController.CreateCompetitor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@EnableCaching
 public class CompetitorController {
     CompetitorLogic competitorLogic;
 
@@ -22,6 +26,7 @@ public class CompetitorController {
     }
 
     @PostMapping("/competitor")
+    @CacheEvict(value = "competitors", allEntries = true)
     public ResponseEntity<Object> createCompetitor(@RequestBody CreateCompetitor competitor) {
         try {
             this.competitorLogic.createCompetitor(competitor.getName(), competitor.getDescription(), competitor.getGameId());
@@ -32,6 +37,7 @@ public class CompetitorController {
     }
 
     @GetMapping("/competitor")
+    @Cacheable("competitors")
     public ResponseEntity<Object> getAllCompetitors() {
         List<Competitor> competitors;
         try {
