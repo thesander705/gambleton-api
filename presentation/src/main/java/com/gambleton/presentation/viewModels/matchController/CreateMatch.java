a222bfc5b8d8c3bcaacaa51df6bcc0849e718a56
Sander de Boer <sander.de.boer4798@hotmail.com>
2018-12-11 14:15:15 +0100

    Created needed viewmodels.

---
+++ presentation/src/main/java/com/gambleton/presentation/viewModels/matchController/CreateMatch.java
@@ -0,0 +1,35 @@
+package com.gambleton.presentation.viewModels.matchController;
+
+import com.gambleton.presentation.viewModels.matchController.createMatch.BetOption;
+
+import java.util.List;
+
+public class CreateMatch {
+    private String title;
+    private String description;
+    private List<BetOption> betOptions;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<BetOption> getBetOptions() {
+        return betOptions;
+    }
+
+    public void setBetOptions(List<BetOption> betOptions) {
+        this.betOptions = betOptions;
+    }
+}
