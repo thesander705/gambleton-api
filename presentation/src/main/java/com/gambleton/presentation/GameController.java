0cd54203d7c19b9a67a501ad5457bc79fce137ac
Unknown <sander.de.boer4798@hotmail.com>
2018-12-14 07:51:49 +0100

    Added possibility to get all matches by game

--- presentation/src/main/java/com/gambleton/presentation/GameController.java
+++ presentation/src/main/java/com/gambleton/presentation/GameController.java
@@ -2,27 +2,28 @@ package com.gambleton.presentation;
 
 import com.gambleton.factory.Factory;
 import com.gambleton.logic.abstraction.GameLogic;
+import com.gambleton.logic.abstraction.MatchLogic;
 import com.gambleton.models.Game;
+import com.gambleton.models.Match;
 import com.gambleton.presentation.viewModels.gameController.CreateGame;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @EnableCaching
 public class GameController {
-    GameLogic gameLogic;
+    private GameLogic gameLogic;
+    private MatchLogic matchLogic;
 
     public GameController() {
         this.gameLogic = Factory.getGameLogic();
+        this.matchLogic = Factory.getMatchLogic();
     }
 
     @PostMapping("/game")
@@ -45,7 +46,20 @@ public class GameController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-        
+
         return ResponseEntity.status(HttpStatus.OK).body(games);
+    }
+
+    @GetMapping("/game/{gameId}/match")
+    @Cacheable(value = "matchesByGame", key = "#gameId")
+    public ResponseEntity<Object> getMatchesByGame(@PathVariable int gameId) {
+        List<Match> matches;
+        try {
+            matches = this.matchLogic.getMatchesByGame(gameId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(matches);
     }
 }
